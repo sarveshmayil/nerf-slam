50581e1497a5a34640752479a73bfbd59e483204
Sarvesh Mayilvahanan <39343912+sarveshmayil@users.noreply.github.com>
2023-04-10 13:13:07 -0400

    Update build.sh

--- docker/cuda11.3/build.sh
+++ docker/cuda11.3/build.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 DOCKER_OPTIONS=""
-DOCKER_OPTIONS+="-f $SCRIPT_DIR/Dockerfile "
+DOCKER_OPTIONS+="-f $SCRIPT_DIR/Dockerfile -t 11.3.0-base-ubuntu20.04:latest"
 # DOCKER_OPTIONS+="--build-arg USER_ID=$(whoami)"
 
 DOCKER_CMD="docker build $DOCKER_OPTIONS $SCRIPT_DIR"
