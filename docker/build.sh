9ada48b405956b0eec08b4dee7c3962138fd038a
Hersh Vakharia <hershv@umich.edu>
2023-04-11 22:15:56 -0400

    fixed missing spacce

--- docker/build.sh
+++ docker/build.sh
@@ -8,7 +8,7 @@ fi
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 DOCKER_OPTIONS=""
-DOCKER_OPTIONS+="-f $SCRIPT_DIR/nerf-slam.Dockerfile -t nerf-slam:cuda11.3-ubuntu20.04"
+DOCKER_OPTIONS+="-f $SCRIPT_DIR/nerf-slam.Dockerfile -t nerf-slam:cuda11.3-ubuntu20.04 "
 DOCKER_OPTIONS+="--build-arg USER_NAME=$(whoami)"
 
 DOCKER_CMD="docker build $DOCKER_OPTIONS $SCRIPT_DIR"
